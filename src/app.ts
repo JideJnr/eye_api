6716c0527b37870e14dd6cfad09d6b79f3239d1f
Olajide Nurudeen <84381571+JideJnr@users.noreply.github.com>
2025-07-24 11:18:53 +0100

    commit

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.use(cors({
   origin: [
     'http://localhost:8100',
     'https://editor.swagger.io',
-    'https://api-football-cy7l.onrender.com',
+    'https://eye-api-abkr.onrender.com',
     'http://localhost:5173'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
