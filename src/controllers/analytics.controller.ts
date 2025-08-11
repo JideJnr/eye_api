8ac057365c9483236f85bd8b4af99bd96e54721a
Olajide Nurudeen <84381571+JideJnr@users.noreply.github.com>
2025-08-11 23:05:52 +0100

    commit

--- src/controllers/analytics.controller.ts
+++ src/controllers/analytics.controller.ts
@@ -1,4 +1,5 @@
 import {  Response , Request} from "express";
+import { sendGetCommand } from "../botClient";
 
 export const getOverview = async ( req: Request, res: Response) => {
   try {
@@ -18,17 +19,30 @@ export const getOverview = async ( req: Request, res: Response) => {
 
 export const getBotMetrics = async ( req: Request, res: Response) => {
   try {
-     res.status(201).json({ 
+     res.status(200).json({ 
       success: true,
       error: 'Failed to start bot service',
       details: 'Unknown error'
     });
   } catch (error) {
-    res.status(201).json({ 
+    res.status(200).json({ 
       success: false,
       error: 'Failed to start bot service',
       details: error instanceof Error ? error.message : 'Unknown error'
     });
   }
 };
+
+  export const getLogs = async (req:Request, res: Response) => {
+    try {
+      const result = await sendGetCommand('start');
+      res.json(result)
+    } catch (error) {
+      res.status(200).json({
+        success: false, 
+        error: 'Failed to start engine service',
+        details: error instanceof Error ? error.message : 'Unknown error'
+      });
+    }
+  };
 
--- src/routes/api/v1/analytics.routes.ts
+++ src/routes/api/v1/analytics.routes.ts
@@ -1,34 +1,10 @@
 import express from 'express';
-import { getBotMetrics, getOverview } from '../../../controllers/analytics.controller';
+import { getBotMetrics, getLogs, getOverview } from '../../../controllers/analytics.controller';
 
 const router = express.Router();
 
-/**
- * @swagger
- * /analytics/overview:
- *   post:
- *     summary: Get general overview analytics
- *     tags: [Analytics]
- *     responses:
- *       200:
- *         description: Overview data returned successfully
- *       500:
- *         description: Server error
- */
+router.get('/logs', getLogs);
 router.post('/overview', getOverview);
-
-/**
- * @swagger
- * /analytics/getmetrics:
- *   get:
- *     summary: Get bot performance metrics
- *     tags: [Analytics]
- *     responses:
- *       200:
- *         description: Bot metrics retrieved successfully
- *       500:
- *         description: Server error
- */
 router.get('/getmetrics', getBotMetrics);
 
 export default router;
