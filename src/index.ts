97477410bae4b6c699180fb143e5475a9767a2ea
Olajide Nurudeen <84381571+JideJnr@users.noreply.github.com>
2025-08-11 17:07:44 +0100

    commit

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import app from './app';
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Neural API is running on http://localhost:${PORT}`);
+  console.log(`🚀 God Eyes API is running on http://localhost:${PORT}`);
 });
